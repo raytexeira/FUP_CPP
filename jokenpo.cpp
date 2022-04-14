857b66d6ecbdb9277c0ea7200fb60c75096b5968
raytexeira <94091664+raytexeira@users.noreply.github.com>
2022-04-14 15:03:15 -0300

    Update jokenpo.cpp

--- jokenpo.cpp
+++ jokenpo.cpp
@@ -3,21 +3,19 @@
 int main(){
 
     char jog1 {}, jog2 {};
-while(true){   
-        std::cin >> jog1 >> jog2;
 
-        if ((jog1 == jog2)) {
-            std::cout << "empate";
-        }
-        else if ((jog1 == 'p') && (jog2 == 'r')) {
-            std::cout << "jog1 win";
-        }
-        else if ((jog1 == 'r') && (jog2 == 's')) {
-            std::cout << "jog1 win";
-        }
-        else if ((jog1 == 's') && (jog2 == 'p')) {
-            std::cout << "jog1 win";
-        }
-        else std::cout << "jog2 winn";
+    std::cin >> jog1 >> jog2;
+    
+    if (jog1 == jog2) {
+        std::cout << "empate\n";
     }
-}
+    else if ((jog1 == 'P') && (jog2 == 'R')) {
+        std::cout << "jog1\n";
+    }
+    else if ((jog1 == 'R') && (jog2 == 'S')) {
+        std::cout << "jog1\n";
+    }
+    else if ((jog1 == 'S') && (jog2 == 'P')) {
+        std::cout << "jog1\n";
+    }
+    else std::cout << "jog2\n";
